bf333a0c13399477f3a581ebbaa20e60af390060
Dusan Vesic <dusan.vesic@devtech.local>
2017-06-02 13:40:17 +0200

    Setup Autoload

--- src/index.php
+++ src/index.php
@@ -2,10 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Utils\Hello;
 $faker = Faker\Factory::create();
 
 echo $faker->name;
 
+echo '<br>';
+echo Hello::run();
+// echo Hello\Hello::run();
 // class Util {
 //     static function hello() {
 //         return 'Hello, world';
